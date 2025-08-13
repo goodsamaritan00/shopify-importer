4821f8f58c25b0c0b1c3cf78eeff07d7e31084b8
goodsamaritan00 <ivansljivic19@gmail.com>
2025-08-14 00:51:29 +0200

    loader

--- src/components/ui/loader.tsx
+++ src/components/ui/loader.tsx
@@ -6,5 +6,11 @@ interface ILoaderProps {
 }
 
 export default function Loader({ size, color }: ILoaderProps) {
-  return <ClipLoader className="absolute top-1/2 left-1/2" size={size} color={color} />;
+return (
+  <ClipLoader
+    className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+    size={size}
+    color={color}
+  />
+);
 }
