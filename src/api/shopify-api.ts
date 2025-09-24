34538667e317c92b3a2cecf98f7aebeac33898b8
ivans <ivansljivic19@gmail.com>
2025-09-25 00:53:48 +0200

    Added pagination for products by devices table, Fixed custom prices adding

--- src/api/shopify-api.ts
+++ src/api/shopify-api.ts
@@ -1,6 +1,6 @@
 import authHeaders from "./utils/auth-headers";
 
-const BASE_URL: string = "http://localhost:5000";
+const BASE_URL: string = "https://importer-be.onrender.com";
 // import product to shopify
 export const importShopifyProduct = async (payload: {
   data: any;
